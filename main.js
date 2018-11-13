67728601201319a1e6708993c0b87b072466b968
cmrios2398 <chrmartinrios@outlook.com>
2018-11-13 16:15:36 +0000

    started on main page

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 
+require('electron-reload')(__dirname);
+ 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
 function createWindow () {
+
+  'use strict';
+ 
+ 
+// Standard stuff
+
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 1000, height: 600})
 
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
@@ -27,7 +35,12 @@ function createWindow () {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', () =>{
+  let mainWindow = new BrowserWindow({width: 1000, height: 600});
+ 
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+  // the rest...
+});
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
