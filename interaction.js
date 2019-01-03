a70f7c8e74e073a583b084c0996d0cb0ab0824f1
cmrios2398 <chrmartinrios@outlook.com>
2019-01-03 14:00:20 +0000

    hiding CDRs and Templates

--- interaction.js
+++ interaction.js
@@ -14,11 +14,52 @@ hideButton.onclick = function() {
 var templateShowButton = document.getElementById('templateButton');
 
 templateShowButton.onclick = function(){
-    var div = document.getElementById('mayHide');
+    var template = document.getElementById('mayHide');
+    var CDR = document.getElementById('mayHide2');
+    if (template.style.display == 'block' && CDR.style.display == 'none') {
+        template.style.display = 'none';
+    }
+    else if(template.style.display == 'none' && CDR.style.display == 'block') {
+        template.style.display = 'block';
+        CDR.style.display = 'none';
+    }
+    else if(template.style.display == 'none' && CDR.style.display == 'none') {
+        template.style.display = 'block';
+    }
+    else{
+        template.style.display = "none";
+    }
+};
+
+var hideButton2 = document.getElementById('hide2'); // Assumes element with id='button'
+
+hideButton2.onclick = function() {
+    // alert("working")
+    var div = document.getElementById('mayHide2');
     if (div.style.display !== 'none') {
         div.style.display = 'none';
     }
     else {
         div.style.display = 'block';
+    }
+};
+
+var CDRShowButton = document.getElementById('CDRButton');
+
+CDRShowButton.onclick = function(){
+    var template = document.getElementById('mayHide');
+    var CDR = document.getElementById('mayHide2');
+    if (CDR.style.display == 'block' && template.style.display == 'none') {
+        CDR.style.display = 'none';
+    }
+    else if(CDR.style.display == 'none' && template.style.display == 'block') {
+        CDR.style.display = 'block';
+        template.style.display = 'none';
+    }
+    else if(CDR.style.display == 'none' && template.style.display == 'none') {
+        CDR.style.display = 'block';
+    }
+    else{
+        CDR.style.display = "none";
     }
 };
