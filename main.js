d46501bfe6ec158aa1964a03096c3226acc930c3
cmrios2398 <chrmartinrios@outlook.com>
2019-03-17 17:17:23 +0000

    window error fixed

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ require('electron-reload')(__dirname);
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow = new BrowserWindow();
+  // let mainWindow = new BrowserWindow();
 
 function createWindow () {
 
